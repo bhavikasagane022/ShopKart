908c86ff51d2eac3502fc4cb73b753a2f8e506dc
Bhavika Sagane <bhavikasagane3@gmail.com>
2026-09-12 15:27:57 +0530

    Fix page imports

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 import Products from "./Pages/Products";
-import Cart from "./pages/Cart";
+import Cart from "./Pages/Cart";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import About from "./Pages/About";
